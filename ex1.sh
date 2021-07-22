bfa4693b655d549cefb886bb1d275705cd5a1564
nizamgms <mdnizam.gms@gmail.com>
2021-07-22 12:32:06 +0000

    commiting all files

---
+++ ex1.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+echo hello
+echo my name is $1
+echo my city is $2
+echo my fine name is $0
---
+++ ex2.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+echo what is your name?
+read name
+echo how are you $name?
+read remark
+echo i am $remark too
